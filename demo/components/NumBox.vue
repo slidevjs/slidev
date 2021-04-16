328f98466424d63a6a0eafe542f5980b07962f15
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-17 07:22:29 +0800

    chore: update

--- demo/components/NumBox.vue
+++ demo/components/NumBox.vue
@@ -14,25 +14,28 @@ const value = useVModel(props, 'value', emit)
 
 <template>
   <div
-    class="w-16 h-16 rounded-xl shadow flex text-white"
+    class="w-16 h-16 rounded-xl shadow text-white overflow-hidden"
     style="background-image: radial-gradient(farthest-corner at 0 0, var(--tw-gradient-from) 30%, var(--tw-gradient-to))"
   >
-    <div class="m-auto text-2xl leading-0.8em" :class="controls ? 'pl-2' : ''">
-      {{ value }}
-    </div>
-    <div v-if="controls" class="grid grid-rows-2 text-xl">
-      <mdi:menu-up-outline
-        class="mt-auto opacity-50 hover:opacity-100 pr-1 cursor-pointer"
-        @click="value += 1"
-      />
-      <mdi:menu-down-outline
-        class="opacity-50 hover:opacity-100 pr-1 cursor-pointer"
-        @click="value -= 1"
-      />
-    </div>
     <div
-      class="absolute w-full h-full rounded-xl ring ring-red-400 opacity-0 transition duration-1000 pointer-events-none"
-      :class="active ? '!opacity-100 !duration-100' : ''"
+      style="background-image: radial-gradient(farthest-corner at 0 0, var(--tw-gradient-from) 30%, var(--tw-gradient-to))"
+      class="absolute w-full h-full from-orange-400 to-red-400 opacity-0 transition duration-600 ease-out pointer-events-none"
+      :class="active ? '!opacity-100 !duration-20' : ''"
     ></div>
+    <div class="absolute w-full h-full flex z-10">
+      <div class="m-auto text-2xl leading-0.8em" :class="controls ? 'pl-2' : ''">
+        {{ value }}
+      </div>
+      <div v-if="controls" class="grid grid-rows-2 text-xl">
+        <mdi:menu-up-outline
+          class="mt-auto opacity-50 hover:opacity-100 pr-1 cursor-pointer"
+          @click="value += 1"
+        />
+        <mdi:menu-down-outline
+          class="opacity-50 hover:opacity-100 pr-1 cursor-pointer"
+          @click="value -= 1"
+        />
+      </div>
+    </div>
   </div>
 </template>
