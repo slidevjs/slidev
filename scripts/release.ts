c9470647fd962fb1b201b8231ebe8105428fe741
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-18 03:36:47 +0800

    chore: release script

---
+++ scripts/release.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { execSync } from 'child_process'
+import { readFileSync, writeFileSync } from 'fs'
+
+function run(command: string) {
+  execSync(command, { stdio: 'inherit' })
+}
+
+run('npx bumpp package.json packages/*/package.json')
+const version = require('../package.json').version
+
+const pkg = JSON.parse(readFileSync('../packages/create-app/template/package.json', 'utf-8'))
+pkg.dependencies['@vite-slides/theme-default'] = version
+pkg.dependencies['vite-slides'] = version
+writeFileSync('../packages/create-app/template/package.json', `${JSON.stringify(pkg, null, 2)}\n`, 'utf-8')
+
+run('git add .')
+run(`git commit -m "chore: release v${version}"`)
+run(`git tag "v${version}"`)
+run('git push --follow-tags')
+run('pnpm -r publish')
