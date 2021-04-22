d3986fa27bc13b5c2bcf3032fb58f752c974f982
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-23 07:29:35 +0800

    chore: update logo

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,7 +6,12 @@
 module.exports = {
   title: 'Slidev',
   description: 'Presentation slides for developers',
-  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['link', { rel: 'dns-prefetch', href: 'https://fonts.gstatic.com' }],
+  ['link', { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.gstatic.com' }],
+  ['link', { href: 'https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@200;400;500;600&display=swap', rel: 'stylesheet' }],
+  ],
   themeConfig: {
     repo: 'antfu/slidev',
     logo: '/logo.svg',
---
+++ .vitepress/theme/index.js
@@ -0,0 +1,6 @@
+import Theme from 'vitepress/theme'
+import './custom.css'
+
+export default {
+  ...Theme,
+}
