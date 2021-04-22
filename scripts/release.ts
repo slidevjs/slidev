6a94e414a7a7d33589888f8e5fe0193bf65b4921
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-23 00:33:09 +0800

    chore: update create-app

--- scripts/release.ts
+++ scripts/release.ts
@@ -11,11 +11,12 @@ const version = require('../package.json').version
 
 const pkg = JSON.parse(readFileSync('packages/create-app/template/package.json', 'utf-8'))
 pkg.dependencies['@slidev/theme-default'] = `^${version}`
-pkg.dependencies.slidev = `^${version}`
+pkg.dependencies['@slidev/cli'] = `^${version}`
 writeFileSync('packages/create-app/template/package.json', `${JSON.stringify(pkg, null, 2)}\n`, 'utf-8')
 
 run('git add .')
 run(`git commit -m "chore: release v${version}"`)
-run(`git tag "v${version}"`)
-run('git push --follow-tags')
+// run(`git tag "v${version}"`)
+// run('git push --follow-tags')
+run('git push')
 run('pnpm -r publish --access public')
