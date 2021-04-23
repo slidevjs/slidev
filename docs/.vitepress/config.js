5c9c54a616e26a5d87e2325aa6d36227b55eed02
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-24 05:15:37 +0800

    chore: release v0.0.0-alpha.9

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   title: 'Slidev',
   description: 'Presentation slides for developers',
   head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
     ['link', { rel: 'dns-prefetch', href: 'https://fonts.gstatic.com' }],
     ['link', { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.gstatic.com' }],
     ['link', { href: 'https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@200;400;500;600&display=swap', rel: 'stylesheet' }],
