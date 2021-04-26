c1ab4838e3cacf09bd3242ab543cfd40b3bc5066
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-26 17:23:36 +0800

    chore: update docs

--- docs/.vitepress/theme/components/ThemeGallery.vue
+++ docs/.vitepress/theme/components/ThemeGallery.vue
@@ -12,7 +12,7 @@ const themes = computed(() => props.collection === 'official' ? official : commu
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-4">
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
     <ThemeInfo v-for="theme of themes" :key="theme.id" :theme="theme" />
   </div>
 </template>
