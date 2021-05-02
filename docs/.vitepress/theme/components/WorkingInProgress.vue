762bf1360b1a0a03217efd72d9628b239a1ee12e
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-02 17:14:01 +0800

    docs: update wip notification

--- docs/.vitepress/theme/components/WorkingInProgress.vue
+++ docs/.vitepress/theme/components/WorkingInProgress.vue
@@ -7,24 +7,29 @@ const dismissed = useStorage('sldiev-docs-wip-dismissed', false)
 <template>
   <div
     v-if="!dismissed"
-    class="fixed top-15 lg:top-2 left-1/2 transform -translate-x-1/2 z-10 min-w-80 max-w-95vw lg:max-w-100"
+    class="fixed top-15 lg:top-2 left-1/2 transform -translate-x-1/2 z-10 w-9/10 md:w-100 backdrop-filter backdrop-blur-sm"
     gap="2"
     flex="~"
     p="l-4 r-2 y-2"
-    text="sm orange-600"
-    bg="orange-100 opacity-90"
+    text="sm orange-500 dark:orange-400"
+    bg="orange-100 opacity-80"
     dark:bg="[#3a281b]"
     border="~ orange-400 rounded-md"
   >
-    <div class="m-auto">Slidev is still under heavy developement. API and usages are not set in stone yet.</div>
-    <carbon:close
+    <div class="m-auto">
+      Slidev is still under heavy developement. API and usages are not set in stone yet.
+    </div>
+    <div
       class="cursor-pointer"
-      text="3xl"
-      hover:bg="orange-400 opacity-10"
+      text="xl"
       border="rounded"
       p="1"
-      h="full"
+      flex="~"
+      opacity="50 hover:100"
+      hover:bg="orange-400 opacity-10"
       @click="dismissed = true"
-    />
+    >
+      <carbon:checkmark class="m-auto"/>
+    </div>
   </div>
 </template>
