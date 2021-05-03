2db3a9154e3ae4f3526d16e8a8ea3d32952ab665
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-04 07:42:20 +0800

    chore: release on CI

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,19 +1,29 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import { objectMap } from '@antfu/utils'
+import { version } from '../package.json'
 import { run, runArgs } from './run'
 
 run('npx bumpp package.json packages/*/package.json --preid alpha')
-const version = require('../package.json').version
 
-const pkg = JSON.parse(readFileSync('packages/create-app/template/package.json', 'utf-8'))
-pkg.dependencies['@slidev/theme-default'] = `^${version}`
-pkg.dependencies['@slidev/theme-seriph'] = `^${version}`
-pkg.dependencies['@slidev/cli'] = `^${version}`
-writeFileSync('packages/create-app/template/package.json', `${JSON.stringify(pkg, null, 2)}\n`, 'utf-8')
+const templates = [
+  'packages/create-app/template',
+  'packages/create-theme/template',
+]
+
+for (const template of templates) {
+  const path = join(template, 'package.json')
+  const pkg = JSON.parse(readFileSync(path, 'utf-8'))
+  pkg.dependencies = objectMap(pkg.dependencies, (k, v) => {
+    if (k.startsWith('@slidev/'))
+      return [k, `^${version}`]
+    return [k, v]
+  })
+  writeFileSync(path, `${JSON.stringify(pkg, null, 2)}\n`, 'utf-8')
+}
 
 run('git add .')
 runArgs('git', ['commit', '-m', `chore: release v${version}`])
-// run(`git tag "v${version}"`)
-// run('git push --follow-tags')
+run(`git tag "v${version}"`)
+run('git push --follow-tags')
 run('git push')
-run('pnpm -r publish --access public')
