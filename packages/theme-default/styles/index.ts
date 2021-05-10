456c13555e6193494a4a19ded169aa5687ca0018
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-11 03:29:39 +0800

    refactor: move themes intro a separated repo

--- packages/theme-default/styles/index.ts
+++
@@ -1,4 +0,0 @@
-import '@slidev/client/styles/layouts-base.css'
-import './layouts.css'
-import './prism.css'
-import './shiki.css'
--- packages/theme-seriph/layouts/section.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="slidev-layout section w-full h-full grid">
-    <div class="my-auto text-center">
-      <slot />
-    </div>
-  </div>
-</template>
