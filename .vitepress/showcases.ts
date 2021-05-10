f88cf40f07aa39820298085955c8a9c05da3f0cb
Seonglae Cho <sungle3737@gmail.com>
2021-05-10 15:44:14 +0900

    docs: add showcase example (#52)

--- .vitepress/showcases.ts
+++ .vitepress/showcases.ts
@@ -25,6 +25,18 @@ export const showcases: ShowCaseInfo[] = [
     at: 'VueDay 2021',
     datetime: '2021-04-29',
   },
+  {
+    title: 'Developer Seonglae',
+    cover: `https://seonglae-slides.vercel.app/og.png`,
+    author: {
+      name: 'Seonglae Cho',
+      link: 'https://github.com/seonglae',
+    },
+    slidesLink: 'https://seonglae-slides.vercel.app',
+    sourceLink: 'https://github.com/seonglae/seonglae-slides',
+    at: 'Seongland',
+    datetime: '2021-05-10',
+  },
   // Add yours here!
   {
     title: 'Yours?',
