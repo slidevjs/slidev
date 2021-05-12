2c3886029cc23e824d9897fc7988ce3326109796
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-13 01:24:50 +0800

    fix: explictly set .light class for light mode, close #87

--- packages/client/logic/dark.ts
+++ packages/client/logic/dark.ts
@@ -1,4 +1,7 @@
 import { useDark, useToggle } from '@vueuse/core'
 
-export const isDark = useDark()
+export const isDark = useDark({
+  valueDark: 'dark',
+  valueLight: 'light',
+})
 export const toggleDark = useToggle(isDark)
