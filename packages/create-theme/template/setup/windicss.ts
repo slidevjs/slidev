8cf6896d1a4285b41e6784818b9f907f9848b6ce
Axel Meinhardt <26243798+ameinhardt@users.noreply.github.com>
2021-05-30 20:55:27 +0200

    fix: windicss scanning templates (#201)

--- packages/create-theme/template/setup/windicss.ts
+++ packages/create-theme/template/setup/windicss.ts
@@ -4,7 +4,7 @@ import { defineWindiSetup } from '@slidev/types'
 export default defineWindiSetup(() => ({
   extract: {
     include: [
-      resolve(__dirname, '**/*.{vue,ts}'),
+      resolve(__dirname, '../**/*.{vue,ts}'),
     ],
   },
   shortcuts: {
