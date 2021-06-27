94b08423765e44b49a6c0aee2fc84f167ae73d68
Anthony Fu <anthonyfu117@hotmail.com>
2021-06-28 06:29:21 +0800

    chore: update deps

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -17,11 +17,16 @@ const { version } = await fs.readJSON('package.json')
 for (const template of templates) {
   const path = join(template, 'package.json')
   const pkg = await fs.readJSON(path)
-  pkg.dependencies = objectMap(pkg.dependencies, (k, v) => {
-    if (k.startsWith('@slidev/') && !k.startsWith('@slidev/theme'))
-      return [k, `^${version}`]
-    return [k, v]
-  })
+  const deps = ['dependencies', 'devDependencies']
+  for (const name of deps) {
+    if (!pkg[name])
+      continue
+    pkg[name] = objectMap(pkg[name], (k, v) => {
+      if (k.startsWith('@slidev/') && !k.startsWith('@slidev/theme'))
+        return [k, `^${version}`]
+      return [k, v]
+    })
+  }
   await fs.writeJSON(path, pkg, { spaces: 2 })
 }
 
