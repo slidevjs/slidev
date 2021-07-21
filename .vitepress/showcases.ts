2afefee64ac67bac36a521f6fcda3a7af165d377
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2021-07-21 05:45:07 +0100

    Fix cover link (#33)

--- .vitepress/showcases.ts
+++ .vitepress/showcases.ts
@@ -81,7 +81,7 @@ export const showcases: ShowCaseInfo[] = [
     at: 'Presentation of my college final project',
     slidesLink: 'https://beapt-presentation.netlify.app',
     sourceLink: 'https://github.com/TutoDS/lei-project/tree/master/presentation',
-    cover: 'https://github.com/TutoDS/lei-project/tree/master/presentation/cover.png',
+    cover: 'https://raw.githubusercontent.com/TutoDS/lei-project/master/presentation/cover.png',
     datetime: '2021-07-20',
   },
   // Add yours here!
