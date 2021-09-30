c28c1324602cdddd19de6a320c21a96a9a5bad2e
Simon Brugman <sbrugman@users.noreply.github.com>
2021-09-30 11:40:27 +0200

    feat: introduce frame layouts

---
+++ packages/client/layouts/iframe.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps, ref } from "vue";
+const props = defineProps({
+  url: {
+    type: String,
+  },
+});
+var url = ref(props.url)
+</script>
+
+<template>
+  <div class="h-full w-full">
+    <div>
+      <iframe id="frame" class="w-full h-full" :src="url"> </iframe>
+    </div>
+  </div>
+</template>
