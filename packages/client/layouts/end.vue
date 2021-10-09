89658638b65e4319575e1ffd2983941e1b9d968c
btea <2356281422@qq.com>
2021-10-09 20:54:35 +0800

    chore: fix typo

--- packages/client/layouts/end.vue
+++ packages/client/layouts/end.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<style scopde lang="postcss">
+<style scoped lang="postcss">
 .slidev-layout.end {
   @apply text-white text-opacity-85 text-xl tracking-widest bg-black h-full text-center grid place-content-center select-none;
 }
