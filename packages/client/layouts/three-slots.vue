1da4b260f7034305319cc57719dcd837300dc498
Enrico <39310565+Leone25@users.noreply.github.com>
2021-10-21 10:34:42 +0200

    Added a center-top, bottom-left-right layout
    Idk really what to name this but i though this would be useful for other people so here we go

---
+++ packages/client/layouts/three-slots.vue
@@ -0,0 +1,38 @@
+
+<!--
+  Usage:
+```md
+---
+layout: two-cols
+---
+# Left
+This shows on the left
+::right::
+# Right
+This shows on the right
+```
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout ">
+    <div :class="props.class">
+      <slot />
+    </div>
+    <div class="two-columns w-full h-full grid grid-cols-2">
+      <div class="col-left" :class="props.class">
+        <slot name="left"/>
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+    </div>
+    </div>
+  </div>
+</template> 
