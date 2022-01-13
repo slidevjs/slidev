50d6db4d1d24f8b027320628581f17a448424c55
Tony CABAYE <tony.cabaye@smile.fr>
2022-01-13 09:37:22 +0100

    refactor: move some computed values inside nav logic file

--- packages/client/builtin/Toc.vue
+++ packages/client/builtin/Toc.vue
@@ -9,9 +9,8 @@ Usage:
 -->
 <script setup lang='ts'>
 import { computed } from 'vue'
-import type { RouteRecordRaw } from 'vue-router'
-import { currentRoute, rawRoutes } from '../logic/nav'
-import type { TocItem } from './TocList.vue'
+import type { TocItem } from '../logic/nav'
+import { tree } from '../logic/nav'
 
 const props = withDefaults(
   defineProps<{
@@ -23,59 +22,18 @@ const props = withDefaults(
   { columns: 1, maxDepth: Infinity, minDepth: 1, mode: 'all' },
 )
 
-function makeTree(): TocItem[] {
-  return rawRoutes
-    .filter((route: RouteRecordRaw) => route.meta?.slide?.title)
-    .reduce((acc: TocItem[], route: RouteRecordRaw) => {
-      addToTree(acc, route)
-      return acc
-    }, [])
-}
-
-function addToTree(tree: TocItem[], route: RouteRecordRaw, level = 1) {
-  const titleLevel = route.meta?.slide?.titleLevel
-  if (titleLevel && titleLevel > level && tree.length > 0) {
-    addToTree(tree[tree.length - 1].children, route, level + 1)
-  }
-  else {
-    tree.push({
-      children: [],
-      level,
-      path: route.path,
-      skipInToc: Boolean(route.meta?.skipInToc),
-      title: route.meta?.slide?.title,
-    })
-  }
-}
-
-function addActiveStatuses(
-  tree: TocItem[],
-  hasActiveParent = false,
-  parent?: TocItem,
-) {
-  tree.forEach((item: TocItem) => {
-    item.active = item.path === currentRoute.value?.path
-    item.hasActiveParent = hasActiveParent
-    if (item.children.length > 0)
-      addActiveStatuses(item.children, item.active || item.hasActiveParent, item)
-    if (parent && (item.active || item.activeParent))
-      parent.activeParent = true
-  })
-}
-
-function filterTree(tree: TocItem[], level = 1): TocItem[] {
+function filterTreeDepth(tree: TocItem[], level = 1): TocItem[] {
   if (level > Number(props.maxDepth)) {
     return []
   }
   else if (level < Number(props.minDepth)) {
     const activeItem = tree.find((item: TocItem) => item.active || item.activeParent)
-    return activeItem ? filterTree(activeItem.children, level + 1) : []
+    return activeItem ? filterTreeDepth(activeItem.children, level + 1) : []
   }
   return tree
-    .filter((item: TocItem) => !item.skipInToc)
     .map((item: TocItem) => ({
       ...item,
-      children: filterTree(item.children, level + 1),
+      children: filterTreeDepth(item.children, level + 1),
     }))
 }
 
@@ -104,19 +62,17 @@ function filterOnlySiblings(tree: TocItem[]): TocItem[] {
 }
 
 const toc = computed(() => {
-  let tree = makeTree()
-  addActiveStatuses(tree)
-  tree = filterTree(tree)
+  let tocTree = filterTreeDepth(tree.value)
   if (props.mode === 'onlyCurrentTree')
-    tree = filterOnlyCurrentTree(tree)
+    tocTree = filterOnlyCurrentTree(tocTree)
   else if (props.mode === 'onlySiblings')
-    tree = filterOnlySiblings(tree)
-  return tree
+    tocTree = filterOnlySiblings(tocTree)
+  return tocTree
 })
 </script>
 
 <template>
   <div :style="{ columnCount: columns }">
-    <toc-list :level="1" :list="toc" />
+    <TocList :level="1" :list="toc" />
   </div>
 </template>
--- packages/client/builtin/TocList.vue
+++ packages/client/builtin/TocList.vue
@@ -1,32 +1,26 @@
 <!--
 TOC list
 (used by Toc component, you don't need to use this component directly)
+
+Usage:
+
+<TocList :list="list"/>
 -->
 <script setup lang="ts">
+import type { TocItem } from '../logic/nav'
 import { go } from '../logic/nav'
 
-export interface TocItem {
-  active?: boolean
-  activeParent?: boolean
-  children: TocItem[]
-  hasActiveParent?: boolean
-  level: number
-  path: string
-  skipInToc?: boolean
-  title?: string
-}
-
-defineProps<{
+withDefaults(defineProps<{
   level: number
   list: TocItem[]
-}>()
+}>(), { level: 1 })
 </script>
 
 <template>
   <ul v-if="list && list.length > 0" :class="['toc', `toc-level-${level}`]">
     <li v-for="item in list" :key="item.path" :class="['toc-item', {'toc-item-active': item.active}, {'toc-item-parent-active': item.activeParent}]">
       <a @click.prevent="go(item.path)">{{ item.title }}</a>
-      <toc-list :level="level + 1" :list="item.children" />
+      <TocList :level="level + 1" :list="item.children" />
     </li>
   </ul>
 </template>
