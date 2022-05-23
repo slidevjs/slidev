9118c4cdc0f4ba9b831f645dd2eb8d37b5fa92d7
htharoldht <31093493+htharoldht@users.noreply.github.com>
2022-05-23 23:49:03 +0800

    feat(mermaid): default dark theme, close #599 (#601)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/client/builtin/Mermaid.vue
+++ packages/client/builtin/Mermaid.vue
@@ -16,6 +16,7 @@ pie
 import { computed, getCurrentInstance, ref, watch, watchEffect } from 'vue'
 import { renderMermaid } from '../modules/mermaid'
 import ShadowRoot from '../internals/ShadowRoot.vue'
+import { isDark } from '../logic/dark'
 
 const props = defineProps<{
   code: string
@@ -25,7 +26,13 @@ const props = defineProps<{
 
 const vm = getCurrentInstance()
 const el = ref<HTMLDivElement>()
-const svgObj = computed(() => renderMermaid(props.code || '', Object.assign({ theme: props.theme }, vm!.attrs)))
+const svgObj = computed(() => renderMermaid(
+  props.code || '',
+  {
+    theme: props.theme || (isDark.value ? 'dark' : undefined),
+    ...vm!.attrs,
+  },
+))
 const html = computed(() => svgObj.value)
 const actualHeight = ref<number>()
 
