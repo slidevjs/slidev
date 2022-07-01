a10a90368574c9a0c0e3db5c6e79682042a5ba1d
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-01 21:44:31 +0800

    chore: fix e2e tests

--- cypress/e2e/examples/basic.spec.ts
+++ cypress/e2e/examples/basic.spec.ts
@@ -35,7 +35,7 @@ context('Basic', () => {
 
     goPage(5)
 
-    cy.get('#page-root > #slide-container > #slide-content > .slidev-page-5 > .slidev-code')
+    cy.get('#page-root > #slide-container > #slide-content > .slidev-page-5 .slidev-code')
       .should('have.text', '<div>{{$slidev.nav.currentPage}}</div>')
       .get('#page-root > #slide-container > #slide-content > .slidev-page-5 > p')
       .should('have.text', 'Current Page: 5')
