e385300710ea3ef7a726c926b1933fd80d40744e
Narcis B <leonardssh22@gmail.com>
2022-07-31 12:49:42 +0200

    fix: dark mode switch (#86)

--- .vitepress/theme/components/DarkModeSwitch.vue
+++ .vitepress/theme/components/DarkModeSwitch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button aria-label="Toggle Theme" class="nav-btn" @click="toggle">
+  <button aria-label="Toggle Theme" class="nav-btn" @click="toggle()">
     <ri-moon-fill v-show="isDark" />
     <ri-sun-fill v-show="!isDark" />
   </button>
