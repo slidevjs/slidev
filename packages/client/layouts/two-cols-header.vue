8c3fcc787be02861b70059e2e940d8ee2f795448
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-09-15 11:03:02 +0100

    feat: add layout of two columns with a header (two-cols-header)

---
+++ packages/client/layouts/two-cols-header.vue
@@ -0,0 +1,39 @@
+<!--
+  Usage:
+```md
+---
+layout: two-cols-header
+---
+This spans both
+::left::
+# Left
+This shows on the left
+::right::
+# Right
+This shows on the right
+```
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout two-columns w-full h-full">
+    <div class="col-span-2">
+      <slot />
+    </div>
+    <div class="grid grid-cols-2">
+      <div class="col-left" :class="props.class">
+        <slot name="left" />
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
