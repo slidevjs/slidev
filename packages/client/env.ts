2f1c24a8548d41e63019dc906cb9beb98bdfbf32
Giacomo Lombardi <lomby92@gmail.com>
2022-10-13 10:51:26 +0200

    code review changes and add test

--- packages/client/env.ts
+++ packages/client/env.ts
@@ -3,20 +3,13 @@ import { computed } from 'vue'
 import { objectMap } from '@antfu/utils'
 // @ts-expect-error missing types
 import _configs from '/@slidev/configs'
+import { getSlideHeightRespectingAspectAsPossible } from './utils'
 
 export const configs = _configs as SlidevConfig
 export const slideAspect = configs.aspectRatio ?? (16 / 9)
 export const slideWidth = configs.canvasWidth ?? 980
 
-// The height must be an integer (apply round) and it should honors the aspect ratio more as possible.
-// Also, to prevent errors on PDF export, we need to increase the height by 1px at time until the real
-// aspect ratio is equal or less than the requested one.
-// Doing this, we will prevent to create empty white pages after all single pages.
-let calculatedHeight = Math.round(slideWidth / slideAspect)
-while (slideAspect < (slideWidth / calculatedHeight))
-  calculatedHeight++
-
-export const slideHeight = calculatedHeight
+export const slideHeight = getSlideHeightRespectingAspectAsPossible(slideWidth, slideAspect)
 
 export const themeVars = computed(() => {
   return objectMap(configs.themeConfig || {}, (k, v) => [`--slidev-theme-${k}`, v])
--- packages/client/utils.ts
+++ packages/client/utils.ts
@@ -6,3 +6,18 @@ export function getSlideClass(route?: RouteRecordRaw) {
     return `slidev-page-${no}`
   return ''
 }
+
+/**
+ * The height must be an integer (apply round) and it should honors the aspect ratio more as possible.
+ * Also, to prevent errors on PDF export, we need to increase the height by 1px at time until the real
+ * aspect ratio is equal or less than the requested one.
+ * Doing this, we will prevent to create empty white pages after all single pages.
+ */
+export function getSlideHeightRespectingAspectAsPossible(requestedWidth: number, aspectRatio: number) {
+  let calculatedHeight = Math.ceil(requestedWidth / aspectRatio)
+
+  while (aspectRatio < (requestedWidth / calculatedHeight))
+    calculatedHeight++
+
+  return calculatedHeight
+}
