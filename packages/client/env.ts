94c199c4489041a0edb05e3acba79240a201f625
Giacomo Lombardi <lomby92@gmail.com>
2022-10-14 14:53:18 +0200

    remove util function

--- packages/client/env.ts
+++ packages/client/env.ts
@@ -3,13 +3,13 @@ import { computed } from 'vue'
 import { objectMap } from '@antfu/utils'
 // @ts-expect-error missing types
 import _configs from '/@slidev/configs'
-import { getSlideHeightRespectingAspectAsPossible } from './utils'
 
 export const configs = _configs as SlidevConfig
 export const slideAspect = configs.aspectRatio ?? (16 / 9)
 export const slideWidth = configs.canvasWidth ?? 980
-
-export const slideHeight = getSlideHeightRespectingAspectAsPossible(slideWidth, slideAspect)
+// To honor the aspect ratio more as possible, we need to approximate the height to the next integer.
+// Doing this, we will prevent on print, to create an additional empty white page after each page.
+export const slideHeight = Math.ceil(slideWidth / slideAspect)
 
 export const themeVars = computed(() => {
   return objectMap(configs.themeConfig || {}, (k, v) => [`--slidev-theme-${k}`, v])
--- packages/client/utils.ts
+++ packages/client/utils.ts
@@ -6,11 +6,3 @@ export function getSlideClass(route?: RouteRecordRaw) {
     return `slidev-page-${no}`
   return ''
 }
-
-/**
- * To honor the aspect ratio more as possible, we need to approximate the height to the next integer.
- * Doing this, we will prevent on print, to create an additional empty white page after each page.
- */
-export function getSlideHeightRespectingAspectAsPossible(requestedWidth: number, aspectRatio: number) {
-  return Math.ceil(requestedWidth / aspectRatio)
-}
