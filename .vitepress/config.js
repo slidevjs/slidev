ecd8bf022f3594c9efceac78aae2852a0055f9bf
Haroen Viaene <fingebimus@me.com>
2022-10-18 04:44:35 +0200

    docs(search): correct credentials for docsearch (#95)

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -252,7 +252,8 @@ module.exports = {
     editLinkText: 'Suggest changes to this page',
 
     algolia: {
-      apiKey: '1a5c5a504139c58f428974c78c55291d',
+      appId: 'LCBV6MIFS6',
+      apiKey: '1ff173fe73b20edc962c1c24c0b1c160',
       indexName: 'slidev',
       searchParameters: {
         // for translations maintainers: change the filter to your locale code (subdomain name)
