37da379f2085eaf0e279e3b3929565606e0df481
Minsu Kim <alstn2468_@naver.com>
2022-10-25 14:49:35 +0900

    docs: Update showcase list (#92)

--- .vitepress/showcases.ts
+++ .vitepress/showcases.ts
@@ -132,6 +132,17 @@ export const showcases: ShowCaseInfo[] = [
     at: 'OpenFunction Tutorial Sharing',
     datetime: '2022-05-08',
   },
+    {
+    title: 'Is it Okay to Pursue Functional Programming on Frontend?',
+    author: {
+      name: 'Minsu Kim , Changhui Lee',
+    },
+    at: '2022 JSConf Korea',
+    slidesLink: 'https://moonlit-nougat-422445.netlify.app/1',
+    sourceLink: 'https://github.com/alstn2468/2022-jsconf-presentation',
+    cover: 'https://raw.githubusercontent.com/alstn2468/2022-jsconf-presentation/main/public/images/og.png',
+    datetime: '2022-09-16',
+  },
   // Add yours here!
   {
     title: 'Yours?',
