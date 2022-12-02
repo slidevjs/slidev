884d527d9499df246fc851afe2aa249b976ece7b
Alexis Renchon <a.renchon@gmail.com>
2022-12-02 10:51:06 -0500

    Update iframe-right.vue

--- packages/client/layouts/iframe-right.vue
+++ packages/client/layouts/iframe-right.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-const props = defineProps<{ class?: string; url: string }>()
+import { computed } from '@vue/reactivity'
+
+const props = defineProps<{
+  url: string
+  scale?: number
+}>()
+
+const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
 </script>
 
 <template>
@@ -7,8 +14,13 @@ const props = defineProps<{ class?: string; url: string }>()
     <div class="slidev-layout default" :class="props.class">
       <slot />
     </div>
-    <div class="h-full">
-      <iframe id="frame" class="w-full h-full" :src="url" />
+    <div relative :style="{ width: scaleInvertPercent, height: scaleInvertPercent }">
+      <iframe
+        id="frame" class="w-full h-full"
+        :src="url"
+        :style="scale ? { transform: `scale(${scale})`, transformOrigin: 'top left' } : {}"
+      />
     </div>
   </div>
 </template>
+ 
