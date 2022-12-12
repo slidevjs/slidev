1d4e2d12eac8015947dffbbc25c961d243948deb
Alexis Renchon <a.renchon@gmail.com>
2022-12-12 15:44:04 -0500

    feat(layouts): scale iframe-left and iframe-right (#783)
    fix https://github.com/slidevjs/slidev/issues/777

--- packages/client/layouts/iframe-left.vue
+++ packages/client/layouts/iframe-left.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
-const props = defineProps<{ class?: string; url: string }>()
+import { computed } from '@vue/reactivity'
+
+const props = defineProps<{
+  url: string
+  scale?: number
+}>()
+
+const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
 </script>
 
 <template>
   <div class="grid grid-cols-2 w-full h-full">
-    <div class="h-full">
-      <iframe id="frame" class="w-full h-full" :src="url" />
-    </div>
+    <div relative :style="{ width: scaleInvertPercent, height: scaleInvertPercent }">
+      <iframe
+        id="frame" class="w-full h-full"
+        :src="url"
+        :style="scale ? { transform: `scale(${scale})`, transformOrigin: 'top left' } : {}"
+      />
     <div class="slidev-layout default" :class="props.class">
       <slot />
     </div>
+    </div>
   </div>
 </template>
+ 
