71e353fac4485bdc6dbbf3f1d2580aec9a1bd943
Anthony Fu <anthonyfu117@hotmail.com>
2023-05-26 21:03:03 +0200

    feat: support `layoutClass` for `two-cols-header` and `two-cols` layouts
    close #950

--- packages/client/layouts/two-cols-header.vue
+++ packages/client/layouts/two-cols-header.vue
@@ -19,21 +19,34 @@ const props = defineProps({
   class: {
     type: String,
   },
+  layoutClass: {
+    type: String,
+  },
 })
 </script>
 
 <template>
-  <div class="slidev-layout two-columns w-full h-full">
-    <div class="col-span-2">
+  <div class="slidev-layout two-cols-header w-full h-full" :class="layoutClass">
+    <div class="col-header">
       <slot />
     </div>
-    <div class="grid grid-cols-2">
-      <div class="col-left" :class="props.class">
-        <slot name="left" />
-      </div>
-      <div class="col-right" :class="props.class">
-        <slot name="right" />
-      </div>
+    <div class="col-left" :class="props.class">
+      <slot name="left" />
+    </div>
+    <div class="col-right" :class="props.class">
+      <slot name="right" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.two-cols-header {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+}
+
+.col-header { grid-area: 1 / 1 / 2 / 3; }
+.col-left { grid-area: 2 / 1 / 3 / 2; }
+.col-right { grid-area: 2 / 2 / 3 / 3; }
+</style>
