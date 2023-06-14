b783b7a67093e56ad1e200e545cf444ef769cec1
Albert Brand <abrand@xebia.com>
2023-06-14 16:50:28 +0200

    Add other tests

--- test/markdown-it-prism.test.ts
+++ test/markdown-it-prism.test.ts
@@ -3,7 +3,10 @@ import Markdown from 'markdown-it'
 import markdownItPrism from '../packages/slidev/node/plugins/markdown-it-prism'
 
 describe('markdown-it-prism', () => {
-  it('should render multi-line strings correctly', () => {
+  const md = Markdown()
+  markdownItPrism(md, { plugins: [], init: () => {} })
+
+  it('should render multi-line JS strings and comments correctly', () => {
     const text = `
 \`\`\`js
 \`Multi
@@ -13,8 +16,31 @@ line\`
  */
 \`\`\`
 `
-    const md = Markdown()
-    markdownItPrism(md, { plugins: [], init: () => {} })
+    const actual = md.render(text)
+    expect(actual).toMatchSnapshot()
+  })
+
+  it('should render multi-line XML nodes correctly', () => {
+    const text = `
+\`\`\`xml
+<a/>
+<b
+  attr="value"
+/>
+\`\`\`
+`
+    const actual = md.render(text)
+    expect(actual).toMatchSnapshot()
+  })
+
+  it('should render non-multiline statements correctly', () => {
+    const text = `
+\`\`\`ts
+const a = 1;
+const b = 2;
+const c = a + b;
+\`\`\`
+`
     const actual = md.render(text)
     expect(actual).toMatchSnapshot()
   })
