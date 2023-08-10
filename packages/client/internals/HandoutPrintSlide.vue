47da6cfac03bc92085b1322a5d22fc9adb702bf0
Oliver-Tobias Ripka <contact@oripka.de>
2023-08-10 18:19:49 +0200

    Adapt margine

--- packages/client/internals/HandoutPrintSlide.vue
+++ packages/client/internals/HandoutPrintSlide.vue
@@ -13,12 +13,12 @@ const route = computed(() => props.route)
 </script>
 
 <template>
-  <div class="pb-12 h-176 flex flex-col break-after-page relative overflow-hidden px-6 mt-104">
+  <div class="mx-6 pb-12 h-176 flex flex-col break-after-page relative overflow-hidden px-6 mt-104">
     <NoteDisplay v-if="route.meta?.slide!.noteHTML" :note-html="route.meta?.slide!.noteHTML" class="grow handout-notes" />
     <div v-else class="grow" />
     <div>
       <HandoutBottom />
-      <div class="text-right mt-3 text-xs ">
+      <div class="absolute bottom-5 right-0 text-right text-[11px] ">
         {{ index + 1 }}
       </div>
     </div>
