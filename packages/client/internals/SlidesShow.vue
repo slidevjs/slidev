6e2af65ad8eceedb96b1c5b4827bb9fe11d8fe0e
Jannchie <jannchie@gmail.com>
2023-09-01 20:20:29 +0900

    feat: view transition support

--- packages/client/internals/SlidesShow.vue
+++ packages/client/internals/SlidesShow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, shallowRef, watch } from 'vue'
+import { computed, ref, shallowRef, watch } from 'vue'
 import { clicks, currentRoute, isPresenter, nextRoute, rawRoutes, router, transition } from '../logic/nav'
 import { getSlideClass } from '../utils'
 import SlideWrapper from './SlideWrapper'
@@ -21,6 +21,49 @@ watch(currentRoute, () => {
     nextRoute.value.meta.__preloaded = true
 }, { immediate: true })
 
+const isViewTransition = ref(false);
+(() => {
+  let finishTransition: undefined | (() => void)
+  let abortTransition: undefined | (() => void)
+  const supportViewTransition = typeof document !== 'undefined' && 'startViewTransition' in document
+
+  router.beforeResolve((from, to) => {
+    if (supportViewTransition && (from.meta.transition === 'view-transition' || to.meta.transition === 'view-transition')) {
+      isViewTransition.value = true
+      const promise = new Promise<void>((resolve, reject) => {
+        finishTransition = resolve
+        abortTransition = reject
+      })
+
+      let changeRoute: () => void
+      const ready = new Promise<void>(resolve => (changeRoute = resolve))
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      const transition = document.startViewTransition(() => {
+        changeRoute()
+        return promise
+      })
+
+      transition.finished.then(() => {
+        abortTransition = undefined
+        finishTransition = undefined
+      })
+      return ready
+    }
+    else {
+      isViewTransition.value = false
+    }
+  })
+
+  router.afterEach(() => {
+    if (finishTransition)
+      finishTransition()
+    if (abortTransition)
+      abortTransition()
+  })
+})()
+
 // preserve the clicks count for previous slide to avoid flash on transition
 let previousClicks: [string | undefined, number] = [] as any
 router.beforeEach(() => {
@@ -39,21 +82,24 @@ const loadedRoutes = computed(() => rawRoutes.filter(r => r.meta?.__preloaded ||
   <GlobalBottom />
 
   <!-- Slides -->
-  <TransitionGroup v-bind="transition" id="slideshow" tag="div">
+  <TransitionGroup v-if="!isViewTransition" v-bind="transition" id="slideshow" tag="div">
     <template v-for="route of loadedRoutes" :key="route.path">
       <SlideWrapper
-        :is="route?.component as any"
-        v-show="route === currentRoute"
+        :is="route?.component as any" v-show="route === currentRoute"
         :clicks="route === currentRoute ? clicks : route.path === previousClicks[0] ? previousClicks[1] : 0"
-        :clicks-elements="route.meta?.__clicksElements || []"
-        :clicks-disabled="false"
-        :class="getSlideClass(route)"
-        :route="route"
-        :context="context"
+        :clicks-elements="route.meta?.__clicksElements || []" :clicks-disabled="false" :class="getSlideClass(route)"
+        :route="route" :context="context"
       />
     </template>
   </TransitionGroup>
-
+  <template v-for="route of loadedRoutes" v-else :key="route.path">
+    <SlideWrapper
+      :is="route?.component as any" v-show="route === currentRoute"
+      :clicks="route === currentRoute ? clicks : route.path === previousClicks[0] ? previousClicks[1] : 0"
+      :clicks-elements="route.meta?.__clicksElements || []" :clicks-disabled="false" :class="getSlideClass(route)"
+      :route="route" :context="context"
+    />
+  </template>
   <!-- Global Top -->
   <GlobalTop />
 
