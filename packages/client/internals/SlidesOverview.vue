0c458dacda631452110fea979a9f93624f3a2a1f
lzt1008 <75012451+lzt1008@users.noreply.github.com>
2023-09-03 04:53:31 +0800

    feat: add open close animation for slides overview (#1090)

--- packages/client/internals/SlidesOverview.vue
+++ packages/client/internals/SlidesOverview.vue
@@ -57,61 +57,60 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div
-    v-show="value"
-    class="slides-overview bg-main !bg-opacity-75 p-16 overflow-y-auto"
+  <Transition
+    enter-active-class="duration-300 ease-out"
+    enter-from-class="opacity-0 scale-102 !backdrop-blur-0px"
+    leave-active-class="duration-300 ease-in"
+    leave-to-class="opacity-0 scale-102 !backdrop-blur-0px"
   >
     <div
-      class="grid gap-y-4 gap-x-8 w-full"
-      :style="`grid-template-columns: repeat(auto-fit,minmax(${cardWidth}px,1fr))`"
+      v-show="value"
+      class="bg-main !bg-opacity-75 p-16 overflow-y-auto backdrop-blur-5px fixed left-0 right-0 top-0 h-[calc(var(--vh,1vh)*100)]"
+      @click="close()"
     >
       <div
-        v-for="(route, idx) of rawRoutes"
-        :key="route.path"
-        class="relative"
+        class="grid gap-y-4 gap-x-8 w-full"
+        :style="`grid-template-columns: repeat(auto-fit,minmax(${cardWidth}px,1fr))`"
       >
         <div
-          class="inline-block border rounded border-opacity-50 overflow-hidden bg-main hover:border-$slidev-theme-primary"
-          :class="{ 'border-$slidev-theme-primary': focus(idx + 1), 'border-gray-400': !focus(idx + 1) }"
-          :style="themeVars"
-          @click="go(+route.path)"
+          v-for="(route, idx) of rawRoutes"
+          :key="route.path"
+          class="relative"
         >
-          <SlideContainer
-            :key="route.path"
-            :width="cardWidth"
-            :clicks-disabled="true"
-            class="pointer-events-none"
+          <div
+            class="inline-block border rounded border-opacity-50 overflow-hidden bg-main hover:border-$slidev-theme-primary transition"
+            :class="{ 'border-$slidev-theme-primary': focus(idx + 1), 'border-gray-400': !focus(idx + 1) }"
+            :style="themeVars"
+            @click="go(+route.path)"
           >
-            <SlideWrapper
-              :is="route.component"
-              v-if="route?.component"
+            <SlideContainer
+              :key="route.path"
+              :width="cardWidth"
               :clicks-disabled="true"
-              :class="getSlideClass(route)"
-              :route="route"
-              context="overview"
-            />
-            <DrawingPreview :page="+route.path" />
-          </SlideContainer>
-        </div>
-        <div
-          class="absolute top-0 opacity-50"
-          :style="`left: ${cardWidth + 5}px`"
-        >
-          {{ idx + 1 }}
+              class="pointer-events-none"
+            >
+              <SlideWrapper
+                :is="route.component"
+                v-if="route?.component"
+                :clicks-disabled="true"
+                :class="getSlideClass(route)"
+                :route="route"
+                context="overview"
+              />
+              <DrawingPreview :page="+route.path" />
+            </SlideContainer>
+          </div>
+          <div
+            class="absolute top-0 opacity-50"
+            :style="`left: ${cardWidth + 5}px`"
+          >
+            {{ idx + 1 }}
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </Transition>
   <button v-if="value" class="fixed text-2xl top-4 right-4 slidev-icon-btn text-gray-400" @click="close">
     <carbon:close />
   </button>
 </template>
-
-<style lang="postcss">
-.slides-overview {
-  @apply fixed left-0 right-0 top-0;
-  backdrop-filter: blur(5px);
-  height: 100vh;
-  height: calc(var(--vh, 1vh) * 100);
-}
-</style>
