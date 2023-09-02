8c8dbc36dea92d62269b56bdf00ddcc75f722340
Jianqi Pan <jannchie@gmail.com>
2023-09-03 05:25:28 +0900

    feat: view transition support (#1101)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

---
+++ packages/client/composables/useViewTransition.ts
@@ -0,0 +1,56 @@
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+export function useViewTransition() {
+  const router = useRouter()
+  const isViewTransition = ref(false)
+
+  let viewTransitionFinish: undefined | (() => void)
+  let viewTransitionAbort: undefined | (() => void)
+
+  const supportViewTransition = typeof document !== 'undefined' && 'startViewTransition' in document
+
+  router.beforeResolve((from, to) => {
+    if (!(from.meta.transition === 'view-transition' || to.meta.transition === 'view-transition')) {
+      isViewTransition.value = false
+      return
+    }
+
+    if (!supportViewTransition) {
+      isViewTransition.value = false
+      console.warn('View transition is not supported in your browser, fallback to normal transition.')
+      return
+    }
+
+    isViewTransition.value = true
+    const promise = new Promise<void>((resolve, reject) => {
+      viewTransitionFinish = resolve
+      viewTransitionAbort = reject
+    })
+
+    let changeRoute: () => void
+    const ready = new Promise<void>(resolve => (changeRoute = resolve))
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const transition = document.startViewTransition(() => {
+      changeRoute()
+      return promise
+    })
+
+    transition.finished.then(() => {
+      viewTransitionAbort = undefined
+      viewTransitionFinish = undefined
+    })
+    return ready
+  })
+
+  if (supportViewTransition) {
+    router.afterEach(() => {
+      viewTransitionFinish?.()
+      viewTransitionAbort?.()
+    })
+  }
+
+  return isViewTransition
+}
--- packages/client/internals/SlidesShow.vue
+++ packages/client/internals/SlidesShow.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { computed, shallowRef, watch } from 'vue'
+import { TransitionGroup, computed, shallowRef, watch } from 'vue'
 import { clicks, currentRoute, isPresenter, nextRoute, rawRoutes, router, transition } from '../logic/nav'
 import { getSlideClass } from '../utils'
+import { useViewTransition } from '../composables/useViewTransition'
 import SlideWrapper from './SlideWrapper'
 
 // @ts-expect-error virtual module
@@ -21,6 +22,8 @@ watch(currentRoute, () => {
     nextRoute.value.meta.__preloaded = true
 }, { immediate: true })
 
+const hasViewTransition = useViewTransition()
+
 // preserve the clicks count for previous slide to avoid flash on transition
 let previousClicks: [string | undefined, number] = [] as any
 router.beforeEach(() => {
@@ -39,7 +42,11 @@ const loadedRoutes = computed(() => rawRoutes.filter(r => r.meta?.__preloaded ||
   <GlobalBottom />
 
   <!-- Slides -->
-  <TransitionGroup v-bind="transition" id="slideshow" tag="div">
+  <component
+    :is="hasViewTransition ? 'div' : TransitionGroup"
+    v-bind="transition"
+    id="slideshow" tag="div"
+  >
     <template v-for="route of loadedRoutes" :key="route.path">
       <SlideWrapper
         :is="route?.component as any"
@@ -52,7 +59,7 @@ const loadedRoutes = computed(() => rawRoutes.filter(r => r.meta?.__preloaded ||
         :context="context"
       />
     </template>
-  </TransitionGroup>
+  </component>
 
   <!-- Global Top -->
   <GlobalTop />
