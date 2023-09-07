c2f4bd6b99be13c24873ef510eaaa3e66091cf9b
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-07 13:11:01 +0200

    fix: `SlideCurrentNo` and `SlidesTotal` context resolving, close #1107

--- packages/client/builtin/SlideCurrentNo.vue
+++ packages/client/builtin/SlideCurrentNo.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import { injectionSlidevContext } from '../constants'
+
+const $slidev = inject(injectionSlidevContext)
+</script>
+
 <template>
   <span>{{ $slidev.nav.currentPage }}</span>
 </template>
--- packages/client/builtin/SlidesTotal.vue
+++ packages/client/builtin/SlidesTotal.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import { injectionSlidevContext } from '../constants'
+
+const $slidev = inject(injectionSlidevContext)
+</script>
+
 <template>
   <span>{{ $slidev.nav.total }}</span>
 </template>
