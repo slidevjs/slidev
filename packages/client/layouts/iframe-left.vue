slidevjs/slidev

commit 1d4e2d12eac8015947dffbbc25c961d243948deb

feat(layouts): scale iframe-left and iframe-right (#783)
fix https://github.com/slidevjs/slidev/issues/777

packages/client/layouts/iframe-left.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
-const props = defineProps<{ class?: string; url: string }>()
+import { computed } from '@vue/reactivity'
+
+const props = defineProps<{
+  url: string
+  scale?: number
+}>()
+
+const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
 </script>
 
 <template>
   <div class="grid grid-cols-2 w-full h-full">
-    <div class="h-full">
-      <iframe id="frame" class="w-full h-full" :src="url" />
-    </div>
+    <div relative :style="{ width: scaleInvertPercent, height: scaleInvertPercent }">
+      <iframe
+        id="frame" class="w-full h-full"
+        :src="url"
+        :style="scale ? { transform: `scale(${scale})`, transformOrigin: 'top left' } : {}"
+      />
     <div class="slidev-layout default" :class="props.class">
       <slot />
     </div>
+    </div>
   </div>
 </template>
+ 

packages/client/layouts/iframe-right.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-const props = defineProps<{ class?: string; url: string }>()
+import { computed } from '@vue/reactivity'
+
+const props = defineProps<{
+  url: string
+  scale?: number
+}>()
+
+const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
 </script>
 
 <template>
@@ -7,8 +14,13 @@ const props = defineProps<{ class?: string; url: string }>()
     <div class="slidev-layout default" :class="props.class">
       <slot />
     </div>
-    <div class="h-full">
-      <iframe id="frame" class="w-full h-full" :src="url" />
+    <div relative :style="{ width: scaleInvertPercent, height: scaleInvertPercent }">
+      <iframe
+        id="frame" class="w-full h-full"
+        :src="url"
+        :style="scale ? { transform: `scale(${scale})`, transformOrigin: 'top left' } : {}"
+      />
     </div>
   </div>
 </template>
+ 

commit 26e5522ed12994408c7f66f3b84e0045f1767611

fix(layouts): show righthand-side of iframe-left
This broke in 1d4e2d1 and resulted in the righthand-side content being
placed outside of the slide.

packages/client/layouts/iframe-left.vue
@@ -17,9 +17,9 @@ const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
         :src="url"
         :style="scale ? { transform: `scale(${scale})`, transformOrigin: 'top left' } : {}"
       />
-      <div class="slidev-layout default" v-bind="$attrs">
-        <slot />
-      </div>
+    </div>
+    <div class="slidev-layout default" v-bind="$attrs">
+      <slot />
     </div>
   </div>
 </template>