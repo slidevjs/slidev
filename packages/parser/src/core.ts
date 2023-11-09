2fcf2c33df671979ff74d47c2ba1bab223bb691b
Mark Skelton <mdskelton99@gmail.com>
2023-11-09 06:57:21 -0600

    Detect yaml

--- packages/parser/src/core.ts
+++ packages/parser/src/core.ts
@@ -58,7 +58,7 @@ function matter(code: string) {
 
   const content = code
     .replace(/^---.*\r?\n([\s\S]*?)---/, replacer)
-    .replace(/^\s*```yml([\s\S]*?)```/, replacer)
+    .replace(/^\s*```ya?ml([\s\S]*?)```/, replacer)
 
   return { data, content }
 }
