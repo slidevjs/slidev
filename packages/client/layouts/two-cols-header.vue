72326d8655532b129b693e13e1dfb7f2fec565c8
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2023-11-10 18:38:27 -0500

    fix(layout): two-cols-header grid with bottom (#1189)

--- packages/client/layouts/two-cols-header.vue
+++ packages/client/layouts/two-cols-header.vue
@@ -11,6 +11,8 @@ This shows on the left
 ::right::
 # Right
 This shows on the right
+::bottom::
+This shows at the bottom, aligned to the end (bottom) of the grid
 ```
 -->
 
@@ -36,6 +38,9 @@ const props = defineProps({
     <div class="col-right" :class="props.class">
       <slot name="right" />
     </div>
+    <div class="col-bottom" :class="props.class">
+      <slot name="bottom" />
+    </div>
   </div>
 </template>
 
@@ -49,4 +54,8 @@ const props = defineProps({
 .col-header { grid-area: 1 / 1 / 2 / 3; }
 .col-left { grid-area: 2 / 1 / 3 / 2; }
 .col-right { grid-area: 2 / 2 / 3 / 3; }
+.col-bottom {
+  align-self: end;
+  grid-area: 3 / 1 / 3 / 3;
+}
 </style>
