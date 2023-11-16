007ed6b8e4a89ef42c40c4190f8e1ed5bb6953a9
Rémi Emonet <twitwi@users.noreply.github.com>
2023-11-16 09:51:50 +0100

    fix: handle slot in v-clicks (#1191)

---
+++ cypress/fixtures/basic/components/WrapInComponentInClicks.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-clicks>
+    <ol style="border: 1px solid green">
+      <li>Point a</li>
+      <decorate-with-li>
+        <slot />
+      </decorate-with-li>
+      <li>Point z</li>
+    </ol>
+  </v-clicks>
+</template>
