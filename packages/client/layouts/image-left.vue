92d488e1bd8db9ef91388f8a0100a37ac2b36e8d
Konv Suu <2583695112@qq.com>
2023-12-09 17:51:02 +0800

    fix: image layout repeat style (#1212)

--- packages/client/layouts/image-left.vue
+++ packages/client/layouts/image-left.vue
@@ -20,7 +20,7 @@ const style = computed(() => handleBackground(props.image, false, props.backgrou
 
 <template>
   <div class="grid grid-cols-2 w-full h-full auto-rows-fr">
-    <div class="w-full w-full" :style="style" />
+    <div class="w-full h-full" :style="style" />
     <div class="slidev-layout default" :class="props.class">
       <slot />
     </div>
