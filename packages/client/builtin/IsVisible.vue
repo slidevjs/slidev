db51dc439bd6a759fff16167503de38811ae0028
Robin Bühler <git+r@obin.ch>
2024-01-28 11:15:10 +0000

    chore: add documentation to IsVisible

--- packages/client/builtin/IsVisible.vue
+++ packages/client/builtin/IsVisible.vue
@@ -1,3 +1,17 @@
+<!--
+Shows it's children only, if the component is visible in the viewport. This is useful for iframes.
+
+Usage:
+
+<IsVisible>
+  Only rendered when in the viewport
+</IsVisible>
+
+<IsVisible>
+  <Youtube id="lkjansdlkjasnd" />
+</IsVisible>
+
+-->
 <script setup lang="ts">
 import { onBeforeUnmount, onMounted, ref } from 'vue'
 
