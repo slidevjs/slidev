6d1d94c35784a88b5a06cc7dad45446976f2cd08
_Kerman <KermanX@qq.com>
2024-02-10 15:08:35 +0800

    feat: new `v-click-gap` component

---
+++ packages/client/builtin/VClickGap.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Fragment, inject, onMounted, watchEffect } from 'vue'
+import { injectionClicks } from '../constants'
+import { makeId, safeParseNumber } from '../logic/utils'
+
+const props = defineProps({
+  size: {
+    type: [String, Number],
+    default: 1,
+  },
+})
+
+const clicksRef = inject(injectionClicks)
+
+onMounted(() => {
+  watchEffect((onCleanup) => {
+    const clicks = clicksRef?.value
+
+    if (!clicks || clicks.disabled)
+      return
+
+    const flowSize = safeParseNumber(props.size)
+    const max = clicks.flowSum + flowSize - 1
+
+    const id = makeId()
+    clicks.register(id, { max, flowSize })
+    onCleanup(() => clicks.unregister(id))
+  })
+})
+</script>
+
+<template>
+  <Fragment />
+</template>
