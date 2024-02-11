a1f0dccf3cd4d0af8d021375ebe6249e52ee15c7
_Kerman <KermanX@qq.com>
2024-02-11 09:40:14 +0800

    feat: add `VClick.props.wrapTextAs`

--- packages/client/builtin/VClick.ts
+++ packages/client/builtin/VClick.ts
@@ -4,6 +4,7 @@
  * Learn more: https://sli.dev/guide/animations.html#click-animations
  */
 
+import type { PropType, VNode } from 'vue'
 import { Text, defineComponent, h } from 'vue'
 import VClicks from './VClicks'
 
@@ -21,6 +22,10 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+    wrapTextAs: {
+      type: Function as PropType<(text: VNode) => VNode>,
+      default: (text: VNode) => h('span', text),
+    },
   },
   render() {
     return h(
@@ -35,7 +40,7 @@ export default defineComponent({
         default: () =>
           this.$slots.default?.().map(v =>
             v.type === Text
-              ? h('span', v)
+              ? this.wrapTextAs(v)
               : v,
           ),
       },
