d03b1ac111aaeca5a498fcc7f1eb905a7a5e2f1a
Anthony Fu <anthonyfu117@hotmail.com>
2024-02-25 12:59:31 +0100

    chore: refactors

--- packages/slidev/node/plugins/loaders.ts
+++ packages/slidev/node/plugins/loaders.ts
@@ -1,4 +1,5 @@
 import { basename, join, resolve } from 'node:path'
+import { builtinModules } from 'node:module'
 import type { Connect, HtmlTagDescriptor, ModuleNode, Plugin, Update, ViteDevServer } from 'vite'
 import { isString, isTruthy, notNullish, objectMap, range } from '@antfu/utils'
 import fg from 'fast-glob'
@@ -648,64 +649,12 @@ defineProps<{ no: number | string }>()`)
       // Copied from https://github.com/microsoft/TypeScript-Website/blob/v2/packages/ata/src/edgeCases.ts
       /** Converts some of the known global imports to node so that we grab the right info */
       function mapModuleNameToModule(moduleSpecifier: string) {
-        // in node repl:
-        // > require("module").builtinModules
-        const builtInNodeMods = [
-          'assert',
-          'assert/strict',
-          'async_hooks',
-          'buffer',
-          'child_process',
-          'cluster',
-          'console',
-          'constants',
-          'crypto',
-          'dgram',
-          'diagnostics_channel',
-          'dns',
-          'dns/promises',
-          'domain',
-          'events',
-          'fs',
-          'fs/promises',
-          'http',
-          'http2',
-          'https',
-          'inspector',
-          'module',
-          'net',
-          'os',
-          'path',
-          'path/posix',
-          'path/win32',
-          'perf_hooks',
-          'process',
-          'punycode',
-          'querystring',
-          'readline',
-          'repl',
-          'stream',
-          'stream/promises',
-          'stream/consumers',
-          'stream/web',
-          'string_decoder',
-          'sys',
-          'timers',
-          'timers/promises',
-          'tls',
-          'trace_events',
-          'tty',
-          'url',
-          'util',
-          'util/types',
-          'v8',
-          'vm',
-          'wasi',
-          'worker_threads',
-          'zlib',
-        ]
-
-        if (builtInNodeMods.includes(moduleSpecifier.replace('node:', '')))
+        if (moduleSpecifier.startsWith('node:'))
+          return 'node'
+        if (builtinModules.includes(moduleSpecifier))
+          return 'node'
+        const mainPackageName = moduleSpecifier.split('/')[0]
+        if (builtinModules.includes(mainPackageName) && !mainPackageName.startsWith('@'))
           return 'node'
 
         // strip module filepath e.g. lodash/identity => lodash
