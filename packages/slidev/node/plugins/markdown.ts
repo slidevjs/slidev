26730635c96270fe43e9aa9de37aaab433c66c0f
_Kerman <kermanx@qq.com>
2024-02-27 20:01:00 +0800

    fix: Katex block clicks animation (#1346)

--- packages/slidev/node/plugins/markdown.ts
+++ packages/slidev/node/plugins/markdown.ts
@@ -154,8 +154,8 @@ export async function createMarkdownPlugin(
 export function transformKaTex(md: string) {
   return md.replace(
     /^\$\$(?:\s*{([\d\w*,\|-]+)}\s*?({.*?})?\s*?)?\n([\s\S]+?)^\$\$/mg,
-    (full, rangeStr = '', options = '', code: string) => {
-      const ranges = (rangeStr as string).split(/\|/g).map(i => i.trim())
+    (full, rangeStr: string = '', options = '', code: string) => {
+      const ranges = !rangeStr.trim() ? [] : rangeStr.trim().split(/\|/g).map(i => i.trim())
       code = code.trimEnd()
       options = options.trim() || '{}'
       return `<KaTexBlockWrapper v-bind="${options}" :ranges='${JSON.stringify(ranges)}'>\n\n\$\$\n${code}\n\$\$\n</KaTexBlockWrapper>\n`
