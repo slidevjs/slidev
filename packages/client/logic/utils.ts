de80f1974624f4d13d7ccd3cd81c98e06090d687
_Kerman <KermanX@qq.com>
2024-03-07 12:41:21 +0800

    fix: id should not contain numbers

--- packages/client/logic/utils.ts
+++ packages/client/logic/utils.ts
@@ -26,7 +26,7 @@ export function useTimer() {
 
 export function makeId(length = 5) {
   const result = []
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
   const charactersLength = characters.length
   for (let i = 0; i < length; i++)
     result.push(characters.charAt(Math.floor(Math.random() * charactersLength)))
