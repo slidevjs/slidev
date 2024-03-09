fd4393bb7bb8b62bd36906c5e62829c94aa923ff
Anthony Fu <anthonyfu117@hotmail.com>
2024-03-09 13:12:31 +0100

    feat(client): introduce the public API file

---
+++ packages/client/public.ts
@@ -0,0 +1,12 @@
+/**
+ * This file is the public APIs that you might use in your app.
+ *
+ * The other files despite they are accessable, are not meant to be used directly, breaking changes might happen.
+ */
+export { useSlideContext } from './context'
+export { useNav } from './composables/useNav'
+export { useDrawings } from './composables/useDrawings'
+export { useDarkMode } from './composables/useDarkMode'
+
+export * from './layoutHelper'
+export * from './env'
