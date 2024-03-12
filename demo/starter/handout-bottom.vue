c8aa9760de51f263cf30bca71a1bdc3277ac6aeb
Oliver-Tobias Ripka <contact@oripka.de>
2024-03-12 17:00:45 +0100

    fix: make examples more apt

--- demo/starter/handout-bottom.vue
+++ demo/starter/handout-bottom.vue
@@ -2,9 +2,9 @@
     <div class="w-full absolute bottom-[75px]">
         <div class="h-[1px] bg-black w-full" />
         <div class="mt-2 relative">
-            <div class="top-3.0 absolute left-0 !text-[11px]">© {{ new Date().getFullYear() }} Author
+            <div class="top-3.0 absolute left-0 !text-[11px]">© {{ new Date().getFullYear() }} Slidev
             </div>
-            <div class="top-3.0 absolute right-0 !text-[11px]">Footer Title - Module - Abc</div>
+            <div class="top-3.0 absolute right-0 !text-[11px]">Presentation slides for developers</div>
         </div>
 
     </div>
--- demo/starter/handout-cover.vue
+++ demo/starter/handout-cover.vue
@@ -7,62 +7,48 @@
             <div class="max-w-md w-full mx-auto">
                 <div class="w-120 mx-auto font-bold flex flex-wrap gap-4">
                     <h1 class="text-5xl font-bold border-b-4 border-gray-800">
-                        Slide Deck Title
+                        Welcome to Slidev
                     </h1>
-                    v0.0
+                    v1.0
                 </div>
                 <div class="h-32"></div>
-                <div class="text-2xl font-bold mx-auto w-120 text-right">Subtitle</div>
+                <div class="text-2xl font-bold mx-auto w-120 text-right">Slidev Starter Template</div>
             </div>
         </div>
         <div class="break-after-page mt-68 ">
 
-            <div class="font-bold text-xl">ATTENTION</div>
-
-            <p class="mt-4">
-                The Information contained in this guide is intended for training purposes only. This guide contains
-                information and activities that, while beneficial for purposes of training in a close, non-production
-                environment, can result in downtime or other severe consequences and therefore are not intended as a
-                reference guide. This guide is not a technical reference and should not, under any circumstances be used
-                in
-                a production environment. Customers should refer to the published specifications applicable to specific
-                products for technical information. The information in this guide is distributed AS IS, and the use of
-                this
-                information or implementation of any recommendations or techniques herein is a customer’s
-                responsibility.
-            </p>
+   
 
             <div class="font-bold text-xl mt-8">COPYRIGHT</div>
 
             <div class="flex flex-row gap-4 mt-4 align-center items-center">
 
 
-                © <span>{{ new Date().getFullYear().toString() }}</span> Author. All rights reserved.
+                © {{ new Date().getFullYear().toString() }} Slidev.
 
             </div>
 
             <p class="mt-4">
-                All other brands and product names are trademarks of their respective owners.</p>
+                Permission is hereby granted, free of charge, to any person obtaining a copy
+                of this software and associated documentation files (the "Software"), to deal
+                in the Software without restriction, including without limitation the rights
+                to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                copies of the Software, and to permit persons to whom the Software is
+                furnished to do so, subject to the following conditions:</p>
             <p class="mt-4">
-                No part of this book covered by copyright may be reproduced in any form or by any means (graphic,
-                electronic, or mechanical, including photocopying, recording, taping, or storage in an electronic
-                retrieval
-                system) without prior written permission of the copyright owner.</p>
+                The above copyright notice and this permission notice shall be included in all
+                copies or substantial portions of the Software.</p>
             <p class="mt-4">
-                The author reserves the right to change any products described herein at any time and
-                without notice. The author
-                assumes no responsibility or liability arising from the use of products or materials described herein,
-                except as
-                expressly agreed to in writing by the author. The use or purchase of this product or
-                materials does not
-                convey a
-                license under any patent rights, trademark rights, or any other intellectual property rights of
-                the author.
+
+                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                SOFTWARE.
             </p>
-            <p class="mt-4">
-                The product described in this manual may be protected by one or more patents, foreign patents, or
-                pending
-                applications.</p>
+
         </div>
     </div>
 </template>
