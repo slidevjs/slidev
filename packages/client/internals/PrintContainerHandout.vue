0a037d66449ad7a68bad3c22097b3afbce21984c
Oliver-Tobias Ripka <contact@oripka.de>
2024-03-12 14:07:47 +0100

    chore: cleanup

--- packages/client/internals/PrintContainerHandout.vue
+++ packages/client/internals/PrintContainerHandout.vue
@@ -1,28 +1,9 @@
 <script setup lang="ts">
 import { parseRangeString } from '@slidev/parser/core'
-import { computed } from 'vue'
-import { provideLocal } from '@vueuse/core'
-import { configs, slideAspect, slideWidth } from '../env'
-import { injectionSlideScale } from '../constants'
 import { useNav } from '../composables/useNav'
 import PrintHandout from './PrintHandout.vue'
 
-const props = defineProps<{
-    width: number
-}>()
-
 const { slides, currentRoute } = useNav()
-
-const width = computed(() => props.width)
-const height = computed(() => props.width / slideAspect.value)
-
-const screenAspect = computed(() => width.value / height.value)
-
-const scale = computed(() => {
-    if (screenAspect.value < slideAspect.value)
-        return width.value / slideWidth.value
-    return (height.value * slideAspect.value) / slideWidth.value
-})
 
 // In print mode, the routes will never change. So we don't need reactivity here.
 let routes = slides.value
@@ -30,16 +11,10 @@ if (currentRoute.value.query.range) {
     const r = parseRangeString(routes.length, currentRoute.value.query.range as string)
     routes = r.map(i => routes[i - 1])
 }
-
-const className = computed(() => ({
-    'select-none': !configs.selectable,
-}))
-
-provideLocal(injectionSlideScale, scale)
 </script>
 
 <template>
-    <div id="print-container" :class="className">
+    <div id="print-container">
         <div id="print-content">
             <PrintHandout v-for="(route, index) of routes" :key="route.path" :route="route" :index="index" />
             <div class="break-after-page h-130" />
--- packages/client/internals/PrintHandout.vue
+++ packages/client/internals/PrintHandout.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
 import type { RouteRecordRaw } from 'vue-router'
 import { computed } from 'vue'
-
 import NoteDisplay from './NoteDisplay.vue'
 
 // @ts-expect-error virtual module
 import HandoutBottom from '/@slidev/global-components/handout-bottom'
 
 const props = defineProps<{ route: RouteRecordRaw; index: number }>()
-
 const route = computed(() => props.route)
 </script>
 
 <template>
   <div class="break-after-page">
-    <!--TODO: A4 specific, figure out better customization-->
+    <!--A4 specific, figure out better customization-->
     <div class="w-full mt-104 h-176 flex flex-col relative overflow-hidden">
       <NoteDisplay v-if="route.meta?.slide!.noteHTML" :note-html="route.meta?.slide!.noteHTML"
         class="w-full mx-auto px-2 handout-notes" />
 
       <div class="">
         <HandoutBottom  :pageNumber="index + 100" />
-        <!-- TODO: I would like to do this in HandoutBottom, but somehow props don't get passed. -->
+        <!-- I would like to do this in HandoutBottom, but somehow props don't get passed. -->
         <div class="absolute bottom-5 right-0 text-right text-[11px] ">
           {{ index + 1 }}
         </div>
