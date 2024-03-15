aea02d928571e87bfe82864378a12218211623fd
vaakian <vaakian@gmail.com>
2024-03-15 22:34:50 +0800

    docs: build demo (#1439)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- scripts/demo.mjs
+++ scripts/demo.mjs
@@ -3,6 +3,7 @@ import { $, cd, fs, path } from 'zx'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const demo = path.resolve(__dirname, '../docs/.vitepress/dist/demo')
 
 await $`npm run build`
 
@@ -11,6 +12,6 @@ if (!fs.existsSync(starterMd))
   await fs.copyFile(path.resolve(__dirname, '../packages/create-app/template/slides.md'), starterMd)
 
 cd(path.resolve(__dirname, '../demo/composable-vue'))
-await $`npx slidev build -d --base /composable-vue/ --out ../../dist/composable-vue`
+await $`npx slidev build --base /demo/composable/ --out ${demo}/composable-vue`
 cd(path.resolve(__dirname, '../demo/starter'))
-await $`npx slidev build -d --base /starter/ --out ../../dist/starter`
+await $`npx slidev build --base /demo/starter/ --out ${demo}/starter`
