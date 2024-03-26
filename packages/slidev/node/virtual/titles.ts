c4673c83987af138d1b5ca470283be88ef725fb1
_Kerman <kermanx@qq.com>
2024-03-26 17:39:41 +0800

    Update packages/slidev/node/virtual/titles.ts
    Co-authored-by: Anthony Fu <github@antfu.me>

--- packages/slidev/node/virtual/titles.ts
+++ packages/slidev/node/virtual/titles.ts
@@ -7,15 +7,13 @@ export const templateTitleRendererMd: VirtualModuleTemplate = {
       .map(({ title }, i) => `<template ${i === 0 ? 'v-if' : 'v-else-if'}="no === ${i + 1}">\n\n${title}\n\n</template>`)
 
     lines.push(
-      [
-        `<script setup lang="ts">`,
-        `import { useSlideContext } from '@slidev/client'`,
-        `import { computed } from 'vue'`,
-        `const props = defineProps<{ no?: number | string }>()`,
-        `const { $page } = useSlideContext()`,
-        `const no = computed(() => +(props.no ?? $page.value))`,
-        `</script>`,
-      ].join('\n'),
+      `<script setup lang="ts">`,
+      `import { useSlideContext } from '@slidev/client'`,
+      `import { computed } from 'vue'`,
+      `const props = defineProps<{ no?: number | string }>()`,
+      `const { $page } = useSlideContext()`,
+      `const no = computed(() => +(props.no ?? $page.value))`,
+      `</script>`,
     )
 
     return lines.join('\n')
