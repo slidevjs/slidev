3a5dea3b80895ca9dcffc2efbf12857a941ff023
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-04 10:56:48 +0200

    feat: use magic-string to do markdown pre-transform

--- packages/slidev/node/syntax/transform/code-wrapper.ts
+++ packages/slidev/node/syntax/transform/code-wrapper.ts
@@ -1,3 +1,4 @@
+import type { MarkdownTransformContext } from 'packages/types'
 import { normalizeRangeStr } from './utils'
 
 export const reCodeBlock = /^```([\w'-]+?)(?:\s*{([\d\w*,\|-]+)}\s*?({.*?})?(.*?))?\n([\s\S]+?)^```$/mg
@@ -5,13 +6,16 @@ export const reCodeBlock = /^```([\w'-]+?)(?:\s*{([\d\w*,\|-]+)}\s*?({.*?})?(.*?
 /**
  * Transform code block with wrapper
  */
-export function transformCodeWrapper(md: string) {
-  return md.replace(
+export function transformCodeWrapper(ctx: MarkdownTransformContext) {
+  ctx.s.replace(
     reCodeBlock,
-    (full, lang = '', rangeStr: string = '', options = '', attrs = '', code: string) => {
+    (full, lang = '', rangeStr: string = '', options = '', attrs = '', code: string, index: number) => {
+      if (ctx.isIgnored(index))
+        return full
       const ranges = normalizeRangeStr(rangeStr)
       code = code.trimEnd()
       options = options.trim() || '{}'
+      ctx.ignores.push([index, index + full.length])
       return `\n<CodeBlockWrapper v-bind="${options}" :ranges='${JSON.stringify(ranges)}'>\n\n\`\`\`${lang}${attrs}\n${code}\n\`\`\`\n\n</CodeBlockWrapper>`
     },
   )
