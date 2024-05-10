f0667e28cc2c7a09df560f1307f738a3125260fc
_Kerman <kermanx@qq.com>
2024-05-11 00:15:08 +0800

    feat: add menu button for `add-entry` command

--- packages/vscode/src/utils/findPossibleEntries.ts
+++ packages/vscode/src/utils/findPossibleEntries.ts
@@ -1,11 +1,13 @@
 import { basename } from 'node:path'
 import { slash } from '@antfu/utils'
 import { workspace } from 'vscode'
+import { projects } from '../projects'
 
 export async function findPossibleEntries() {
   const files = await workspace.findFiles('**/*.md', '**/{node_modules,.github}/**')
   return files
     .map(uri => slash(uri.fsPath))
+    .filter(path => !projects.has(path))
     .filter(path => ![
       'readme.md',
       'code_of_conduct.md',
