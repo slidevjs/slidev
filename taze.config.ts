9bedb59fcfd95c91035c941f8838ec57d185250e
Anthony Fu <github@antfu.me>
2024-05-25 12:51:08 +0200

    chore: update deps

--- taze.config.ts
+++ taze.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'taze'
 export default defineConfig({
   packageMode: {
     codemirror: 'minor',
+    // v9 drops v18.0, we keep using v8 until we bump the deps
+    execa: 'minor',
   },
 })
