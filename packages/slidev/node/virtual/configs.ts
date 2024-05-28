754d77b7c0776cf0b5998d8b59b93be8d28f8912
_Kerman <kermanx@qq.com>
2024-05-28 10:56:07 +0800

    fix: should render title correctly in `index.html`

--- packages/slidev/node/virtual/configs.ts
+++ packages/slidev/node/virtual/configs.ts
@@ -1,22 +1,14 @@
 import { isString } from '@antfu/utils'
-import { stringifyMarkdownTokens } from '../utils'
+import { getSlideTitle } from '../commands/shared'
 import type { VirtualModuleTemplate } from './types'
 
 export const templateConfigs: VirtualModuleTemplate = {
   id: '/@slidev/configs',
   getContent: async ({ data, remote }, { md }) => {
-    function getTitle() {
-      if (isString(data.config.title)) {
-        const tokens = md.parseInline(data.config.title, {})
-        return stringifyMarkdownTokens(tokens)
-      }
-      return data.config.title
-    }
-
     const config = {
       ...data.config,
       remote,
-      title: getTitle(),
+      slidesTitle: getSlideTitle(data),
     }
 
     if (isString(config.info))
