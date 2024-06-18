469aad3bae04d5477477b2f1353930b81d14c93b
_Kerman <kermanx@qq.com>
2024-06-18 18:08:33 +0800

    chore: update

--- taze.config.ts
+++ taze.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     codemirror: 'minor',
     // v9 drops v18.0, we keep using v8 until we bump the deps
     execa: 'minor',
-    //
+    // See #1537
     typeit: 'ignore',
   },
 })
