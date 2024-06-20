65171423bfa61bf8717e4325051b324a0a7e600f
_Kerman <kermanx@qq.com>
2024-06-21 00:15:05 +0800

    feat: add `PoweredBySlidev` component and `i-slidev-logo`

---
+++ packages/client/builtin/PoweredBySlidev.vue
@@ -0,0 +1,8 @@
+<template>
+  <span inline-flex items-center>
+    <span text-main>Powered by</span>
+    <a href="https://sli.dev" class="!border-none">
+      <img alt="Slidev logo" src="../assets/logo-title-horizontal.png" h-1.5em>
+    </a>
+  </span>
+</template>
