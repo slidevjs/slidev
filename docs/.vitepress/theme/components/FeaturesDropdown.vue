c5e4b2f9a502a37ccd1b2fa346bb278e6d07e913
_Kerman <kermanx@qq.com>
2024-07-11 10:44:44 +0800

    update dropdown style

--- docs/.vitepress/theme/components/FeaturesDropdown.vue
+++ docs/.vitepress/theme/components/FeaturesDropdown.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import VPMenuLink from 'vitepress/dist/client/theme-default/components/VPMenuLink.vue'
 import { data as features } from '../../../features/index.data'
+
+defineProps<{
+  screenMenu: boolean
+}>()
 
 const allFeatures = {
   name: 'all',
@@ -13,8 +18,21 @@ const allFeatures = {
 </script>
 
 <template>
-  <div>
-    <a href="/features/"> ✨ All Features </a>
-  </div>
-  <FeaturesOverview :features="Object.values(features).concat(allFeatures)" style="min-width: 50vw" @scroll.prevent />
+  <VPMenuLink :item="{ text: '✨ All Features', link: '/features/' }" />
+  <template v-if="screenMenu">
+    <VPMenuLink v-for="feature in Object.values(features)" :key="feature.name" :item="{ text: feature.title, link: feature.link }" />
+  </template>
+  <FeaturesOverview v-else :features="Object.values(features).concat(allFeatures)" class="features" />
 </template>
+
+<style scoped>
+.features {
+  width: 710px;
+}
+
+@media (max-width: 1050px) {
+  .features {
+    width: 470px;
+  }
+}
+</style>
