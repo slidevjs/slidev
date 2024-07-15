49c925cbd1d1c9e2b27233312fbde39d994d9026
_Kerman <kermanx@qq.com>
2024-07-15 21:33:31 +0800

    fix

--- docs/.vitepress/theme/components/Layout.vue
+++ docs/.vitepress/theme/components/Layout.vue
@@ -7,20 +7,19 @@ const { Layout } = DefaultTheme
 
 <template>
   <Layout>
-    <template #nav-bar-content-before>
-      <Tooltip
-        :triggers="['hover', 'click']"
-        :popper-triggers="['hover']"
-      >
-        <Badge class="scale-80 translate-x--1 select-none">
-          New Docs
-        </Badge>
-        <template #popper>
-          You are visiting the new Slidev documentation.<br>
-          The old one is available
-          <a href="https://docs-legacy.sli.dev/" class="underline text-primary">here</a>.
-        </template>
-      </Tooltip>
+    <template #nav-bar-title-after>
+      <a @click.prevent>
+        <Tooltip :triggers="['hover', 'click']" :popper-triggers="['hover']">
+          <Badge class="scale-80 translate-x--1 select-none">
+            New Docs
+          </Badge>
+          <template #popper>
+            You are visiting the new Slidev documentation.<br>
+            The old one is available
+            <a href="https://docs-legacy.sli.dev/" class="underline text-primary">here</a>.
+          </template>
+        </Tooltip>
+      </a>
     </template>
   </Layout>
 </template>
