9f49243b185abbc34ea261b97f7981855e0e5238
_Kerman <kermanx@qq.com>
2024-08-16 13:12:16 +0800

    fix: should call `createDataUtils` correctly

--- packages/slidev/node/vite/loaders.ts
+++ packages/slidev/node/vite/loaders.ts
@@ -44,7 +44,7 @@ export function createSlidesLoader(
 
   let skipHmr: { filePath: string, fileContent: string } | null = null
 
-  const { data, clientRoot, roots, mode, utils } = options
+  const { data, mode, utils } = options
 
   function getSourceId(index: number, type: 'md' | 'frontmatter') {
     return `${data.slides[index].source.filepath}__slidev_${index + 1}.${type}`
@@ -200,7 +200,7 @@ export function createSlidesLoader(
       }
 
       Object.assign(data, newData)
-      Object.assign(utils, createDataUtils(newData, clientRoot, roots))
+      Object.assign(utils, createDataUtils(options))
 
       if (hmrSlidesIndexes.size > 0)
         moduleIds.add(templateTitleRendererMd.id)
