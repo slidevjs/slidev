e451480d69c6568de2862bcda69e92fad08ec7fe
Timothy Yin <master@uniiem.com>
2024-08-28 01:39:11 +0800

    fix: word count errors in client (#1845)
    Add Chinese character set for word count

--- packages/client/pages/overview.vue
+++ packages/client/pages/overview.vue
@@ -49,7 +49,18 @@ function toggleRoute(route: SlideRoute) {
 }
 
 function wordCount(str: string) {
-  return str.match(/[\w`'\-]+/g)?.length || 0
+  const pattern = /[\w`'\-\u0392-\u03c9\u00c0-\u00ff\u0600-\u06ff\u0400-\u04ff]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g
+  const m = str.match(pattern)
+  let count = 0
+  if (!m) return 0
+  for (let i = 0; i < m.length; i++) {
+    if (m[i].charCodeAt(0) >= 0x4e00) {
+      count += m[i].length
+    } else {
+      count += 1
+    }
+  }
+  return count
 }
 
 function isElementInViewport(el: HTMLElement) {
