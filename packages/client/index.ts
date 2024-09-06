d77ac036bff6487a528333b833ab060dbf9625f8
Tony CABAYE <tony.cabaye@smile.fr>
2024-09-06 18:50:51 +0200

    feat: create extendable sync states

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -9,5 +9,11 @@ export { useDrawings } from './composables/useDrawings'
 export { useDarkMode } from './composables/useDarkMode'
 export { useIsSlideActive, onSlideEnter, onSlideLeave } from './logic/slides'
 
+export { createSyncState, disableSlidevSync, addSyncMethod } from './state/syncState'
+export { onDrawingUpdate, drawingState } from './state/drawings'
+export { onSharedUpdate, sharedState } from './state/shared'
+export type { DrawingsState } from './state/drawings'
+export type { SharedState } from './state/shared'
+
 export * from './layoutHelper'
 export * from './env'
