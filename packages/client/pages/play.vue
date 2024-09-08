b8935244c2b0470c730ab5258650f23b08df49d0
Modupe Akanni <modupe775@gmail.com>
2024-09-08 15:10:34 +0100

    fix: accessibility navigation

--- packages/client/pages/play.vue
+++ packages/client/pages/play.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import { computed, ref, shallowRef } from 'vue'
 import { useStyleTag } from '@vueuse/core'
-import { editorHeight, editorWidth, isEditorVertical, isScreenVertical, showEditor, windowSize } from '../state'
-import { useSwipeControls } from '../composables/useSwipeControls'
-import { registerShortcuts } from '../logic/shortcuts'
-import Controls from '../internals/Controls.vue'
-import SlideContainer from '../internals/SlideContainer.vue'
-import NavControls from '../internals/NavControls.vue'
-import SlidesShow from '../internals/SlidesShow.vue'
-import PrintStyle from '../internals/PrintStyle.vue'
-import { onContextMenu } from '../logic/contextMenu'
-import { useNav } from '../composables/useNav'
-import { useWakeLock } from '../composables/useWakeLock'
+import { computed, ref, shallowRef } from 'vue'
 import { useDrawings } from '../composables/useDrawings'
+import { useNav } from '../composables/useNav'
+import { useSwipeControls } from '../composables/useSwipeControls'
+import { useWakeLock } from '../composables/useWakeLock'
+import Controls from '../internals/Controls.vue'
+import NavControls from '../internals/NavControls.vue'
 import PresenterMouse from '../internals/PresenterMouse.vue'
+import PrintStyle from '../internals/PrintStyle.vue'
+import SlideContainer from '../internals/SlideContainer.vue'
+import SlidesShow from '../internals/SlidesShow.vue'
+import { onContextMenu } from '../logic/contextMenu'
+import { registerShortcuts } from '../logic/shortcuts'
+import { editorHeight, editorWidth, isEditorVertical, isScreenVertical, showEditor, windowSize } from '../state'
 
 const { next, prev, isPrintMode } = useNav()
 const { isDrawing } = useDrawings()
@@ -80,7 +80,7 @@ if (__DEV__ && __SLIDEV_FEATURE_EDITOR__)
       <template #controls>
         <div
           v-if="!isPrintMode"
-          class="absolute bottom-0 left-0 transition duration-300 opacity-0 hover:opacity-100"
+          class="absolute bottom-0 left-0 transition duration-300 opacity-0 hover:opacity-100 focus-within:opacity-100 focus-visible:opacity-100"
           :class="[
             persistNav ? '!opacity-100 right-0' : 'opacity-0 p-2',
             isDrawing ? 'pointer-events-none' : '',
