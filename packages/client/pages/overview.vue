6c97d5ca7d0ad89728825b5a54d49d73ebee313a
Timothy Yin <master@uniiem.com>
2024-09-27 15:14:24 +0800

    fix: Chinese character counting error in overview page (#1845) (#1846)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/client/pages/overview.vue
+++ packages/client/pages/overview.vue
@@ -49,7 +49,20 @@ function toggleRoute(route: SlideRoute) {
 }
 
 function wordCount(str: string) {
-  return str.match(/[\w`'\-]+/g)?.length || 0
+  const pattern = /[\w`'\-\u0392-\u03C9\u00C0-\u00FF\u0600-\u06FF\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4DBF\uF900-\uFAFF\u3040-\u309F\uAC00-\uD7AF]+/g
+  const m = str.match(pattern)
+  let count = 0
+  if (!m)
+    return 0
+  for (let i = 0; i < m.length; i++) {
+    if (m[i].charCodeAt(0) >= 0x4E00) {
+      count += m[i].length
+    }
+    else {
+      count += 1
+    }
+  }
+  return count
 }
 
 function isElementInViewport(el: HTMLElement) {
