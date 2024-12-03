c65cb827ee8857a2d47a9b76dd82b4279034b653
_Kerman <kermanx@qq.com>
2024-12-03 23:38:15 +0800

    add comments

--- packages/client/uno.config.ts
+++ packages/client/uno.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   safelist: [
     '!opacity-0',
     'prose',
+    // See https://github.com/slidevjs/slidev/issues/1705
     'grid-rows-[1fr_max-content]',
     'grid-cols-[1fr_max-content]',
   ],
