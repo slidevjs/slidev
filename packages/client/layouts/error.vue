16c58f2ac106b948bf5b04142f0cc94ad90c84b8
_Kerman <kermanx@qq.com>
2024-12-04 14:17:20 +0800

    fix: improve error message in SPA mode (#1973)

--- packages/client/layouts/error.vue
+++ packages/client/layouts/error.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="px-4 py-10 text-center text-red-700 dark:text-red-500 font-bold font-mono">
-    An error occurred on this slide. Check the terminal for more information.
+    {{
+      __SLIDEV_HAS_SERVER__
+        ? 'An error occurred on this slide. Check the terminal for more information.'
+        : 'Failed to fetch this slide. Please check your network connection.'
+    }}
   </div>
 </template>
