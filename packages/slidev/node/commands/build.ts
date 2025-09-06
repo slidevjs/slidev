5414c67c093ea7738e34ca98ab87efdd36ef2428
pinage404 <pinage404@gmail.com>
2025-09-06 17:54:09 +0200

    feat: auto generated open graph image respect `--timeout` option
    Relates to #2265

--- packages/slidev/node/commands/build.ts
+++ packages/slidev/node/commands/build.ts
@@ -94,7 +94,7 @@ export async function build(
         height: Math.round(options.data.config.canvasWidth / options.data.config.aspectRatio),
         routerMode: options.data.config.routerMode,
         waitUntil: 'networkidle',
-        timeout: 30000,
+        timeout: args.timeout || 30000,
         perSlide: true,
         omitBackground: false,
       })
