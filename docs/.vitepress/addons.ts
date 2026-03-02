d0f50648cd1aaa2fa33294dd152286a650e57234
Mark Olson <mjvo@users.noreply.github.com>
2026-03-01 19:33:56 -0500

    Add p5js runner addon to addons list
    Would welcome inclusion of my p5js code runner on the list of slidev addons!  
    
    https://www.npmjs.com/package/slidev-addon-p5

--- docs/.vitepress/addons.ts
+++ docs/.vitepress/addons.ts
@@ -225,6 +225,17 @@ export const community: AddonInfo[] = [
     },
     repo: 'https://github.com/emaarco/slidev-addon-bpmn',
   },
+  {
+    id: 'slidev-addon-p5',
+    name: 'Runner for p5js',
+    description: 'Edit and run p5js sketches in your slidev',
+    tags: ['tool', 'Code runner'],
+    author: {
+      name: 'mjvo',
+      link: 'https://github.com/mjvo',
+    },
+    repo: 'https://github.com/slidev-addon-p5',
+  },
   // Add yours here!
   {
     id: '',
