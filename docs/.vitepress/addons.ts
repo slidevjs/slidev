41a2d70dad72858edb67dbdf4c778e313d2a4ed2
Xun Zhang <xun.zhang0506@gmail.com>
2026-04-03 14:57:29 +0800

    docs: add slidev-pane addon to addons list (#2496)
    Co-authored-by: _Kerman <kermanx@qq.com>

--- docs/.vitepress/addons.ts
+++ docs/.vitepress/addons.ts
@@ -247,6 +247,17 @@ export const community: AddonInfo[] = [
     },
     repo: 'https://github.com/mjvo/slidev-addon-p5',
   },
+  {
+    id: 'slidev-pane',
+    name: 'slidev-pane',
+    description: 'PowerPoint-style pane presenter for Slidev with slide thumbnails, quick navigation, and a zoomable main canvas.',
+    tags: ['Navigation'],
+    author: {
+      name: 'xunz',
+      link: 'https://github.com/xunz3',
+    },
+    repo: 'https://github.com/xunz3/slidev-pane',
+  },
   // Add yours here!
   {
     id: '',
