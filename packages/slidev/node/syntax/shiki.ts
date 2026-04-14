c19764215df91d18f967de22bf9653ca764176ee
_kerman <kermanx@qq.com>
2026-04-14 11:55:50 +0800

    fix

--- packages/slidev/node/syntax/shiki.ts
+++ packages/slidev/node/syntax/shiki.ts
@@ -2,7 +2,6 @@ import type { ResolvedSlidevOptions } from '@slidev/types'
 import type { ShikiTransformer } from 'shiki'
 import { isTruthy } from '@antfu/utils'
 import { fromAsyncCodeToHtml } from '@shikijs/markdown-it/async'
-import { transformerTwoslashConditional } from '../transform/twoslash-conditional'
 
 export default async function MarkdownItShiki({ data: { config }, mode, utils: { shiki, shikiOptions } }: ResolvedSlidevOptions) {
   async function getTwoslashTransformer() {
@@ -39,4 +38,23 @@ export default async function MarkdownItShiki({ data: { config }, mode, utils: {
     ...shikiOptions,
     transformers,
   })
+}
+
+// Fix #2202
+function transformerTwoslashConditional(): ShikiTransformer {
+  return {
+    name: 'slidev:twoslash-conditional',
+    code: function applyConditionalFlag(this: any, node) {
+      if (node.tagName === 'v-menu') {
+        if (node.properties[':shown'] === 'true')
+          node.properties[':shown'] = '$nav.currentPage === $page'
+      }
+      else {
+        for (const child of node.children) {
+          if (child.type === 'element')
+            applyConditionalFlag(child)
+        }
+      }
+    },
+  }
 }
